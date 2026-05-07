c6aee51c0cd1e8b03124545600625ee2deec7ac9
whatIsAUsernameidk <aubschmutz@outlook.com>
2026-05-07 09:17:47 -0600

    added a kill zone for npcs

--- game-jam/Npc/human_npc.gd
+++ game-jam/Npc/human_npc.gd
@@ -122,6 +122,10 @@ func get_index_of_a_point(point:Vector2):
 	pass
 
 
+func die():
+	queue_free()
+
+
 func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
 	velocity = safe_velocity
 	move_and_slide()
