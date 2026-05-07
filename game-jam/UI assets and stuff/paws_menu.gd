31df07c7751ca68ed2e591c3062281f7f18eefd9
cam <camdenadams1013@gmail.com>
2026-05-07 10:11:13 -0600

    AAAAAAAAAAAAAAAAAAAAAA

--- game-jam/UI assets and stuff/paws_menu.gd
+++ game-jam/UI assets and stuff/paws_menu.gd
@@ -1,16 +1,16 @@
 extends Control
 
-
+@onready var transition = $transition/AnimationPlayer
 # Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	#REMOVE THIS
+	$Panel/VBoxContainer/Button.grab_focus()
+	$transition.visible = false
 
 
 func _on_quit_pressed() -> void:
+	$transition.visible = true
+	transition.play("fade_out")
 	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://UI assets and stuff/menu_of_mainness.tscn")
